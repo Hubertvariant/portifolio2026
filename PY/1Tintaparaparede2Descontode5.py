c0572b2fdab3343239f74cf34af8ddfaeab1a5a9
Hubertvariant <hubert.ramos@escola.pr.gov.br>
2026-02-27 10:42:11 -0300

    Add files via upload

--- PY/1Tintaparaparede2Descontode5.py
+++ PY/1Tintaparaparede2Descontode5.py
@@ -1,6 +1,7 @@
 print('Menu de atividades')
 print('1) Tinta para parede')
 print('2) Desconto de 5%')
+print('3) Conversão de Temperaturas')
 ex = int(input('Digite uma opção: '))
 
 if ex == 1:
@@ -15,3 +16,8 @@ elif ex == 2:
     prod = int(input('Digite o valor do produto: '))
     print(f'O valor do produto com 5% de desconto é: {prod / 100 * 5}')
 
+elif ex == 3:
+    print('3) Conversão de Temperaturas')
+    c = float(input('Digite a temperatura em Celsius: '))
+    f = c * 1.8 + 32
+    print(f'{c}°C corresponde a {f}°F')
