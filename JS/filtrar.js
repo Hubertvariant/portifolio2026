3e51d0a6735d2cfa4f12ac80ca35a7bff080c7eb
Hubertvariant <hubert.ramos@escola.pr.gov.br>
2026-03-17 08:41:53 -0300

    Update filtrar.js

--- JS/filtrar.js
+++ JS/filtrar.js
@@ -1,10 +1,10 @@
-const usuarios = [
+const usuarios = [ //Lista (array)
     {nome: "carlos", idade: 32},
-    {nome: "ana", idade: 28},
+    {nome: "Ana", idade: 28},
     {nome: "felipe", idade: 40}
 ]
 
-
+// encontrando um objeto / o find vai buscar / === exato
 const ana = usuarios.find(usuarios => usuarios.nome === "ana")
 console.log(ana)
 
@@ -13,3 +13,6 @@ console.log(usuariosAcimaDe30)
 
 const usuariosOrdenadosPorIdade = usuarios.sort((a, b) => a.idade - b.idade)
 console.log(usuariosOrdenadosPorIdade)
+
+const usuariosOrdenadosAoContrarioPorIdade = usuarios.sort((a, b) => b.idade - a.idade)
+console.log(usuariosOrdenadosAoContrarioPorIdade)
